365803b8249202f216d6b79c6dc22958d43b6c0d
Milan <milan.prouteau@hotmail.fr>
2020-05-05 17:08:46 +0200

    [Milan Prouteau] update api.js

--- api/api.js
+++ api/api.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const PeopleService = require('./people-service');
+const PeopleService = require("./people-service");
 const peopleService = new PeopleService();
 const app = express();
+const v1 = express.Router();
 
-// To be implemented!
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/v1", v1);
+
+v1.put('/people/:id', async (request, response) => {
+    const id = request.params.id;
+    const object = request.body;
+
+    const people = peopleService.updatePeople(id, object);
+
+    if (people) {
+        response.sendStatus(HttpStatus.OK);
+    } else {
+        response.sendStatus(HttpStatus.NOT_FOUND);
+    }
+});
+
+v1.get('/people', async (request, response) => {
+    const filter = request.query;
+    filter ? response.send(peopleService.getPeople(filter)) : response.sendStatus(HttpStatus.NOT_FOUND);
+});
 
 module.exports = app;
